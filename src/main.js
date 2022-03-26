01cc0668f35c38cd2a7d091d652707992be3be38
sdcard2008 <100673474+sdcard2008@users.noreply.github.com>
2022-03-26 15:05:58 +0530

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 function showSomeAction() {
-    $.getJSON("http://ip-api.com/json" , function (data) {
-        $("div#practical").html("<div id='ip'><p id='ip'>" + data.query + "</p><p id = ip-data> Your country: " + data.country + "<br/> Region: " + data.region + "<br/> City: " + data.city + "</p>")
+    $.getJSON("https://ipapi.co/json/" , function (data) {
+        $("div#practical").html("<div id='ip'><p id='ip'>" + data.ip + "</p><p id = ip-data> Your country: " + data.country_name + "<br/> Region: " + data.region + "<br/> City: " + data.city + "</p>")
         
     })
 }
